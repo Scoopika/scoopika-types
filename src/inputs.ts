3ec770f558aeb70f68ee172f5099ccd0df18e921
Kais Radwan <kais.radwan.personal@gmail.com>
2024-04-28 04:28:04 -0400

    Improved the Inputs type

--- src/inputs.ts
+++ src/inputs.ts
@@ -1,3 +1,9 @@
 export type Input = string | number | boolean | Array<string | number | boolean>;
 
-export type Inputs = Record<string, Input>;
+export type Inputs = Record<string, Input> & {
+  message?: string;
+  plug?: {
+    rag?: string | ((context: string) => string | Promise<string>);
+    images?: string[];
+  }
+};
