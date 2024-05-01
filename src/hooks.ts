db27ec9ce4fea64862664e0c0ca89247bf110f71
Kais Radwan <kais.radwan.personal@gmail.com>
2024-05-01 04:31:59 -0400

    Updated hooks

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,6 +1,8 @@
-import { StreamMessage } from "./agents";
+import { AgentResponse, StreamMessage } from "./agents";
 
 export interface Hooks {
   onStream?: (stream: StreamMessage) => any;
   onToken?: (token: string) => any;
+  onFinish?: (response: AgentResponse) => any;
+  onStart?: (run_id: string) => any;
 }
